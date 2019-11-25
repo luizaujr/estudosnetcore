d066a0ad94f6056ce07c07b5dadad78f371d7d72
luizaujr <luizaujr@gmail.com>
2019-11-25 13:41:00 -0400

    Adicionando projeto da calculadora

---
+++ CalculadoraCore/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace CalculadoraCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string option = "N";
+
+            while(option.ToUpper() != "Y")
+            {
+                Console.Clear();
+
+                // Declare variables and then initialize to zero.
+                int num1 = 0; int num2 = 0;
+
+                // Display title as the C# console calculator app.
+                Console.WriteLine("Console Calculator in C#\r");
+                Console.WriteLine("------------------------\n");
+
+                // Ask the user to type the first number.
+                Console.WriteLine("Type a number, and then press Enter");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                // Ask the user to type the second number.
+                Console.WriteLine("Type another number, and then press Enter");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                // Ask the user to choose an option.
+                Console.WriteLine("Choose an option from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("Your option? ");
+
+                // Use a switch statement to do the math.
+                switch (Console.ReadLine())
+                {
+                    case "a":
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                        break;
+                    case "s":
+                        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                        break;
+                    case "m":
+                        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                        break;
+                    case "d":
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                        break;
+                }
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'Y' to exit, or any other key to continue.");
+                option = Console.ReadLine().ToUpper();
+            }
+        }
+    }
+}
